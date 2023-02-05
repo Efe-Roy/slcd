88c93f5157a542e3282337ca07f2721e7def7ffd
Efe-Roy <dakaraefe2@gmail.com>
2023-02-05 15:00:15 +0100

    railway add

--- slcd_core/settings.py
+++ slcd_core/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join[ BASE_DIR/ 'static' ]
-STATIC_ROOT = os.path.join[ BASE_DIR/ 'staticfiles' ]
+STATICFILES_DIRS = [ BASE_DIR/ 'static' ]
+STATIC_ROOT = "staticfiles"
 
 AUTH_USER_MODEL = 'accounts.User'
 
